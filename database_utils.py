eae3dba006f7e2058e4a15962cf1d1cf18a4e4cd
SBailey123 <solenne.bailey19@gmail.com>
2023-11-13 19:58:17 +0100

    Creates the DB Connector

--- database_utils.py
+++ database_utils.py
@@ -1,16 +1,31 @@
 import yaml
+from sqlalchemy import create_engine, text, inspect
+import pandas as pd
+import psycopg2
 
 
 class DatabaseConnector:
-    # def __init__(self):
-    #     return self
+    def __init__(self, file_path):
+        self.file_path = file_path
     
-    def read_db_creds(file_path):
-        with open(file_path, 'r') as file:
+    def read_db_creds(self):
+        with open(self.file_path, 'r') as file:
             data = yaml.safe_load(file)
         return data
     
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        db_url = f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+        engine = create_engine(db_url)
+        return engine
+    
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        table_list = inspector.get_table_names()
+        print(table_list)
 
-Data_dict = DatabaseConnector.read_db_creds("db_creds.yaml")
-print(Data_dict)
+DBC = DatabaseConnector("db_creds.yaml")
 
+#  DBC.init_db_engine()
+DBC.list_db_tables()
