1b6c4f1d8ae3c81a35ef10bd11001fb4e61ab522
SBailey123 <adambreeze99@gmail.com>
2023-11-15 12:55:09 +0100

    adds read table

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,13 @@
-class DataExtractor:
-    
+import pandas as pd
+from database_utils import DatabaseConnector
+
+
+class DataExtraction:
+    def read_rds_table(self, table_name, engine):
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql_query(query, con=engine, index_col="index")
+        return df
+
+if __name__ == "__main__":
+    DBC = DatabaseConnector("db_creds.yaml")
+    DE = DataExtraction(DBC)
