5878a6aa30d228fa640102ff6b2f6044ca1946cf
SBailey123 <adambreeze99@gmail.com>
2023-11-16 17:53:37 +0100

    atemps to retrieve stores data

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from database_utils import DatabaseConnector
 import tabula
+import store_header
+import requests
 
 database_connector = DatabaseConnector()
 
@@ -16,3 +18,16 @@ class DataExtraction:
         list_of_df = tabula.read_pdf(path_to_pdf, pages="all")
         df = pd.concat(list_of_df, ignore_index=True)
         return df
+    
+    def list_number_of_stores(self, url="https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header=store_header.header_dict):
+        respose = requests.get(url=url, headers=header)
+        return respose.text
+    
+    def retrieve_stores_data(self, header=store_header.header_dict):
+        for store_number in list(range(451)):
+            url = f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
+            response = requests.get(url=url, headers=header)
+            store_data_json = response.json()
+            store_data_df = pd.json_normalize(store_data_json)
+            return store_data_df
+    
--- main.py
+++
@@ -1,7 +0,0 @@
-from database_utils import DatabaseConnector
-from data_extraction import DataExtraction
-from data_cleaning import DataCleaning
-
-engine = database_connector.init_db_engine()
-
-users_df = data_extractor.read_rds_table("legacy_users", engine)
