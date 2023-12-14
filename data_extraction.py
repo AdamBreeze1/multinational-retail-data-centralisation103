6a55ac773ac0543eea97094fe1fd98556b6192a7
Adam Breeze <adambreeze@gmail.com>
2023-12-14 19:17:05 +0000

    Updates based on engineer feedback

--- data_extraction.py
+++ data_extraction.py
@@ -6,37 +6,73 @@ import requests
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
+import json
 
 
 class DataExtraction:
     def read_rds_table(self, table_name):
-        database_connector = DatabaseConnector()
+        '''
+        Reads the table and returns a DataFrame
+
+        Parameters:
+        table_name (string): table name you want to read 
+
+        Returns:
+        pd.DataFrame: of the table
+        '''
         with database_connector.init_db_engine().connect() as con:
             query = f"SELECT * FROM {table_name}"
             df = pd.read_sql_query(query, con=con, index_col="index")
             return df
         
-    def retrieve_pdf_data(self, path_to_pdf="https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"):
+    def retrieve_pdf_data(self, path_to_pdf):
+        '''
+        Reads a table in a pdf and returns a DataFrame
+
+        Parameters:
+        path_to_pdf (string): the url for the pdf table you want to read
+
+        Returns:
+        pd.DataFrame: of the table
+        '''
         list_of_df = tabula.read_pdf(path_to_pdf, pages="all")
         df = pd.concat(list_of_df, ignore_index=True)
         return df
     
-    def list_number_of_stores(self, url="https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header=store_header.header_dict):
-        respose = requests.get(url=url, headers=header)
-        return respose.text
+    def list_number_of_stores(self, url, header=store_header.api_key):
+        '''
+        Returns the number of stores from the url
+
+        parameters:
+        url (string): url of the data for the stores number
+
+        Returns: 
+        int: the number of stores as an integer
+        '''
+        response = requests.get(url=url, headers=header)
+        data = response.json()
+        number_stores = data['number_stores']
+        return int(number_stores)
     
-    def retrieve_stores_data(self, header=store_header.header_dict):
+    def retrieve_stores_data(self, data_url_path, number_stores_url, header=store_header.api_key):
         '''
         Retrieves stores dat line by line.
         ***Takes a long time (10 minutes)***
+
+        Parameters:
+        - number_stores_url (string): url for the stores data.
+        - data_url_path (string): url for the count of stores.
+
+        Returns:
+        pd.DataFrame: df of all the stores data
         '''
         # Create an empty DataFrame to store the results
         all_store_data_df = pd.DataFrame()
 
-        for store_number in range(451):
-            url = f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
+        for store_number in range(self.list_number_of_stores(number_stores_url)):
+            url = data_url_path + str(store_number)
             response = requests.get(url=url, headers=header)
-
+            
             # Check if the request was successful (status code 200)
             if response.status_code == 200:
                 store_data_json = response.json()
@@ -47,13 +83,8 @@ class DataExtraction:
 
         return all_store_data_df
     
-    def extract_from_s3(self):
+    def extract_from_s3(self, bucket_name, object_name, local_file_path):
         s3 = boto3.client("s3", region_name="eu-west-1", config=Config(signature_version=UNSIGNED))
-
-        # Specify the S3 bucket name, object key, and local file path
-        bucket_name = "data-handling-public"
-        object_name = "products.csv"
-        local_file_path = "original_dfs/original_products.csv"
 
         # Download the file from S3 to the local file path
         s3.download_file(bucket_name, object_name, local_file_path) 
--- database_utils.py
+++ database_utils.py
@@ -1,25 +1,44 @@
 import yaml
-from sqlalchemy import create_engine, text, inspect
-import pandas as pd
-import psycopg2
+from sqlalchemy import create_engine, inspect
 
 
 class DatabaseConnector:
-    def __init__(self, creds_file_path = "db_creds.yaml"):
+    def __init__(self, creds_file_path):
+        '''
+        Initializes the database connector
+
+        Parameters:
+        creds_file_path (string): the file name containing the credentials
+        '''
         self.creds_file_path = creds_file_path
     
     def read_db_creds(self):
+        '''
+        Reads the credentials initialized in "__init__"
+
+        Returns:
+        yaml: credentials needed for the database engine
+        '''
         with open(self.creds_file_path, 'r') as file:
             data = yaml.safe_load(file)
         return data
     
     def init_db_engine(self):
+        '''
+        Initializes the engine to link to the original database
+
+        Returns:
+        Engine: created to link to database
+        '''
         creds = self.read_db_creds()
         db_url = f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
         engine = create_engine(db_url)
         return engine
     
     def list_db_tables(self):
+        '''
+        Lists the table names in the database
+        '''
         engine = self.init_db_engine()
         inspector = inspect(engine)
         table_list = inspector.get_table_names()
@@ -33,13 +52,10 @@ class DatabaseConnector:
         - df (pd.DataFrame): DataFrame to be uploaded.
         - table_name (str): Name of the destination database table.
         '''
-        creds = self.read_db_creds()
-        db_url = f"postgresql+psycopg2://{creds['sales_data_USER']}:{creds['sales_data_PASSWORD']}@{creds['sales_data_HOST']}:{creds['sales_data_PORT']}/{creds['sales_data_DATABASE']}"
-        engine = create_engine(db_url)
+        engine = self.init_db_engine()
         df.to_sql(table_name, engine)
             
             
-
 if __name__ == "__main__":
     DBC = DatabaseConnector()
     DBC.init_db_engine()
